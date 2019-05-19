5de019c8b815b12e10ca234c7cc53e0f74c36ebe
Shangshu Cai <shangshu.cai@uber.com>
2019-05-19 13:54:40 -0700

    use global variable to process asynchronized fetch forecast result

--- redux-reactors-boilerplate/src/reactors/forecastReactor.js
+++ redux-reactors-boilerplate/src/reactors/forecastReactor.js
@@ -1,20 +1,20 @@
 import {createReactor} from 'redux-reactors';
 
+let periods = [{}];
+
 export const getForecastReactor = createReactor('GET_FORECAST', (state, action) => {
+
+  fetch('https://api.weather.gov/gridpoints/MTR/87,118/forecast')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      console.log(myJson);
+      periods = myJson.properties.periods;
+    });
+
   return {
     ...state,
-    periods:[{number: 1,
-      name: "Today",
-      startTime: "2019-05-19T11:00:00-07:00",
-      endTime: "2019-05-19T18:00:00-07:00",
-      isDaytime: true,
-      temperature: 58,
-      temperatureUnit: "F",
-      temperatureTrend: "falling",
-      windSpeed: "9 to 14 mph",
-      windDirection: "WSW",
-      icon: "https://api.weather.gov/icons/land/day/tsra_hi,20/tsra_hi,70?size=medium",
-      shortForecast: "Slight Chance Showers And Thunderstorms",
-      detailedForecast: "A slight chance of showers and thunderstorms before 3pm, then showers and thunderstorms likely between 3pm and 4pm, then a slight chance of showers and thunderstorms between 4pm and 5pm, then a chance of rain. Partly sunny. High near 58, with temperatures falling to around 56 in the afternoon. West southwest wind 9 to 14 mph, with gusts as high as 20 mph. Chance of precipitation is 70%. New rainfall amounts less than a tenth of an inch possible."}],
+    periods: periods
   };
 });
