98e7ed5a1ee221ab49828a8161732fe21d411bb3
Shangshu Cai <shangshu.cai@uber.com>
2019-05-19 14:33:13 -0700

    use base ui to beautify the display of forecast component

--- redux-reactors-boilerplate/src/components/forecastCard.js
+++ redux-reactors-boilerplate/src/components/forecastCard.js
@@ -1,18 +1,25 @@
 
 import React from 'react';
+import {Card, StyledBody, StyledThumbnail} from 'baseui/card';
+
 
 // This is an example of create react componenet by using function.
 export const ForecastCard = (props) => {
   const {name, temperature, temperatureUnit, windSpeed, windDirection, icon} = props.period;
 
   return (
-    <div key={name}>
-      <img src={icon}></img>
-      <li>name: {name}</li>
-      <li>temperature: {temperature}</li>
-      <li>temperatureUnit: {temperatureUnit}</li>
-      <li>windSpeed: {windSpeed}</li>
-      <li>windDirection: {windDirection}</li>
-    </div>
+    <Card
+      overrides={{Root: {style: {width: '328px'}}}}
+      title={name}
+      key={name}
+    >
+      <StyledThumbnail src={icon}/>
+      <StyledBody>
+        <li>temperature: {temperature}</li>
+        <li>temperatureUnit: {temperatureUnit}</li>
+        <li>windSpeed: {windSpeed}</li>
+        <li>windDirection: {windDirection}</li>
+      </StyledBody>
+    </Card>
   );
 }
