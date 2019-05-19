e93dedb5a928d850e46c119cd392fb6046176dfb
Shangshu Cai <shangshu.cai@uber.com>
2019-05-19 12:24:45 -0700

    Display the default weather to the weather component

--- redux-reactors-boilerplate/src/pages/weather.js
+++ redux-reactors-boilerplate/src/pages/weather.js
@@ -8,10 +8,25 @@ class Weather extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    this.props.getForecast();
+  }
+
   render() {
+    const {periods} = this.props;
+    if (!periods) {
+      return null;
+    }
+
+    const {name, temperature, temperatureUnit, windSpeed, windDirection, icon} = periods[0];
     return (
       <div>
-        <h2>This is Weather Page</h2>
+        <li>name: {name}</li>
+        <li>temperature: {temperature}</li>
+        <li>temperatureUnit: {temperatureUnit}</li>
+        <li>windSpeed: {windSpeed}</li>
+        <li>windDirection: {windDirection}</li>
+        <img src={icon}></img>
       </div>
     );
   }
